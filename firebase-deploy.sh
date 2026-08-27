84e6585908b63e8101a39666296aa3d669d46908
Willy Samuel Paz Colque <willypaz243@gmail.com>
2026-08-26 21:56:40 -0400

    chore: update .gitignore and firebase-deploy.sh

--- firebase-deploy.sh
+++ firebase-deploy.sh
@@ -1,7 +1,7 @@
 # firebase section
-mkdir -p public/data/FCyT/2026-01
-cp data/FCyT/2026-01/*.json public/data/FCyT/2026-01/.
-cp data/FCyT/2026-01.json public/data/FCyT/.
+mkdir -p public/data/FCyT/2026-02
+cp data/FCyT/2026-02/*.json public/data/FCyT/2026-02/.
+cp data/FCyT/2026-02.json public/data/FCyT/.
 cp data/FCyT/index.json public/data/FCyT/.
 cp data/FCyT/news.json public/data/FCyT/.
 cp data/index.json public/data/.
